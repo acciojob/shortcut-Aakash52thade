79dc440aaf75f51d7b8587ca6127506905642827
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-08-06 19:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// function shortcut(s1, s2) {
-//   // your code here
-// 	// 
-// }
-
-// // Do not change the code below.
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(shortcut(s1, s2));
-function shortcut(s1, s2){
-	let word = str.split('');
-	return word[0];
+function shortcut(s1, s2) {
+    if (s1.length === 0 || s2.length === 0) {
+        return '';
+    }
+    return s1[0] + s2[0];
 }
+
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
